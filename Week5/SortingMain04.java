b9ac6f564fa48a69a907d1342a40f82ee51ba2cd
Aisyatul <aisyatulhuriyah08@gmail.com>
2026-04-10 15:39:42 +0700

    Selection Sorting

--- Week5/SortingMain04.java
+++ Week5/SortingMain04.java
@@ -3,13 +3,21 @@ package Week5;
 public class SortingMain04 {
     public static void main(String[] args) {
         int[] a = {34, 7, 23, 32, 5, 62};
+        int[] b = {30, 20, 2, 8, 14};
 
         Sorting04 sorting = new Sorting04(a);
+        Sorting04 sorting2 = new Sorting04(b);
 
         System.out.println("Original Array : ");
         sorting.print();
         sorting.bubbleSort();
         System.out.println("Sorted array (Bubble Sort) : ");
         sorting.print();
+
+        System.out.println("Original Array : ");
+        sorting2.print();
+        sorting2.bubbleSort();
+        System.out.println("Sorted array (Selection Sort) : ");
+        sorting2.print();
     }
 }
