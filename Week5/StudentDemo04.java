9c93dcee83fa0ee18afa582935157ddcbe5702c2
Aisyatul <aisyatulhuriyah08@gmail.com>
2026-04-10 15:58:49 +0700

    Sorting Student Data Based on GPA (Bubble Sort)

---
+++ Week5/StudentDemo04.java
@@ -0,0 +1,20 @@
+package Week5;
+
+public class StudentDemo04 {
+    public static void main(String[] args) {
+        TopStudents04 topStudents = new TopStudents04(5);
+
+        topStudents.add(new Student04("2201", "Alice", "A", 3.9));
+        topStudents.add(new Student04("2202", "Bob", "B", 3.7));
+        topStudents.add(new Student04("2203", "Charlie", "C", 3.8));
+        topStudents.add(new Student04("2204", "David", "D", 3.6));
+        topStudents.add(new Student04("2205", "Eve", "E", 4.0));
+
+        System.out.println("Original student list: ");
+        topStudents.print();
+
+        topStudents.bubbleSort();
+        System.out.println("Sorted student list (by GPA, descending): ");
+        topStudents.print();
+    }
+}
---
+++ Week5/TopStudents04.java
@@ -0,0 +1,38 @@
+package Week5;
+
+public class TopStudents04 {
+    Student04[] listStudents;
+    int idx = 0;
+
+    public TopStudents04(int size) {
+        listStudents = new Student04[size];
+        idx = 0;
+    }
+
+    public void add(Student04 student) {
+        if (idx < listStudents.length) {
+            listStudents[idx] = student;
+            idx++; 
+        } else {
+            System.out.println("List is full!");
+        }
+    }
+
+    public void print() {
+        for (int i = 0; i < idx; i++) {
+            listStudents[i].print(); 
+        }
+    }
+
+    public void bubbleSort() {
+        for (int i = 0; i < idx - 1; i++) {
+            for (int j = 0; j < idx - i - 1; j++) {
+                if (listStudents[j].gpa < listStudents[j + 1].gpa) { 
+                    Student04 temp = listStudents[j];
+                    listStudents[j] = listStudents[j + 1];
+                    listStudents[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
