355a74f3c402a62e6b7a8329d03242b62c22a74e
Aisyatul <aisyatulhuriyah08@gmail.com>
2026-05-03 21:11:07 +0700

    original code

---
+++ Jobsheet10_Stack/ConversionStack04.java
@@ -0,0 +1,41 @@
+package Jobsheet10_Stack;
+
+public class ConversionStack04 {
+    int[] binaryStack;
+    int size;
+    int top;
+
+    public ConversionStack04() {
+        this.size = 32;        
+        binaryStack = new int[size];
+        top = -1;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public boolean isFull() {
+        return top == size - 1;
+    }
+
+    public void push(int data) {
+        if (isFull()) {
+            System.out.println("Stack is already full!");
+        } else {
+            top++;
+            binaryStack[top] = data;
+        }
+    }
+
+    public int pop() {
+        if (isEmpty()) {
+            System.out.println("Stack is still empty!");
+            return -1;
+        } else {
+            int data = binaryStack[top];
+            top--;
+            return data;
+        }
+    }
+}
