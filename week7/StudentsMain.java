9744a87a2b24e0f309b743c8d326928d3fab3de2
AisyatulHuriyah <118157908+AisyatulHuriyah@users.noreply.github.com>
2026-05-29 21:47:55 +0700

    upload ulang

---
+++ week7/StudentsMain.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class StudentsMain {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int amountStudent = 5;
+        SearchStudent data = new SearchStudent(amountStudent);
+
+        System.out.println("Input student data accordingly from smallest NIM");
+        for (int i = 0; i < amountStudent; i++) {
+            System.out.println("---------------------------");
+            System.out.print("NIM\t: ");
+            int nim = s.nextInt();
+            s.nextLine();
+            System.out.print("Name\t: ");
+            String name = s.nextLine();  
+            System.out.print("Age\t: ");
+            int age = s.nextInt();
+            s.nextLine();
+            System.out.print("GPA\t: ");
+            double gpa = s.nextDouble();
+            s.nextLine();
+
+            Students std = new Students(nim, age, name, gpa);
+            data.add(std);
+        }
+
+        System.out.println("---------------------------");
+        System.out.println("Entire Student Data");
+        data.display();
+
+        System.out.println("___________________________");
+        System.out.println("___________________________");
+        System.out.print("Search student by NIM: ");
+        int search = s.nextInt();
+        System.out.println("Using Sequentaial Search");
+        int position = data.findSeqSearch(search);
+
+        data.showPosition(search, position);
+
+        data.showData(search, position);
+    }
+
+}
