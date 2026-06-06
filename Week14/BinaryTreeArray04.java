6107a29336ff56619784cfa30e220d74b32119a4
Aisyatul <aisyatulhuriyah08@gmail.com>
2026-06-06 17:16:47 +0700

    Assignment no 4

--- Week14/BinaryTreeArray04.java
+++ Week14/BinaryTreeArray04.java
@@ -23,4 +23,26 @@ public class BinaryTreeArray04 {
             }
         }
     }
+
+    // Menambah data ke array
+    public void add(Student04 data) {
+        if (idxLast + 1 >= this.data.length) {
+            System.out.println("Tree array is full!");
+            return;
+        }
+        this.data[++idxLast] = data;
+    }
+
+    // Traversal pre-order
+    public void traversePreOrder() {
+        traversePreOrder(0);
+    }
+
+    void traversePreOrder(int idxStart) {
+        if (idxStart <= idxLast && data[idxStart] != null) {
+            data[idxStart].print();
+            traversePreOrder(2 * idxStart + 1);
+            traversePreOrder(2 * idxStart + 2);
+        }
+    }
 }
