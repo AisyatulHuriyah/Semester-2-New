b9998409373cc98136a213f5824dcfcabaae3f70
Aisyatul <aisyatulhuriyah08@gmail.com>
2026-06-06 12:43:13 +0700

    original code experiment 1

---
+++ Week14/Node04.java
@@ -0,0 +1,15 @@
+package Week14;
+
+public class Node04 {
+    Student04 data;
+    Node04 left;
+    Node04 right;
+
+    Node04() {
+    }
+    Node04(Student04 data) {
+        this.data = data;
+        left = null;
+        left = null;
+    }
+}
